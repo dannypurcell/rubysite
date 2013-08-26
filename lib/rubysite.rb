2ac4969d52d76de9184b52d9cade73c123926a46
Danny Purcell <dpurcelljr@gmail.com>
2013-08-26 00:26:00 -0500

    Adds Bootstrap layout, wip conversion

--- lib/rubysite.rb
+++ lib/rubysite.rb
@@ -36,7 +36,10 @@ module Rubysite
       Sinatra::Base::set :root, Proc.new { File.join(File.expand_path(File.dirname(__FILE__)), 'rubysite') }
       Sinatra::Base::set :public_folder, Proc.new { File.join(File.expand_path(File.dirname(__FILE__)), 'rubysite', 'public') }
       Sinatra::Base::set :views, Proc.new { File.join(File.expand_path(File.dirname(__FILE__)), 'rubysite', 'views') }
-      Sinatra::Base::set :css_files, {default: File.join(File.expand_path(File.dirname(__FILE__)), 'rubysite', 'public', 'css', 'default.css')}
+      Sinatra::Base::set :app_name, Proc.new {
+        app_name_arg = args.select { |arg| arg.include?("app_name") }.first || "app_name=#{base.to_s}"
+        app_name_arg.split(/\=|\s/).last
+      }
 
       Rubysite.define_routes(base)
 
@@ -46,6 +49,14 @@ module Rubysite
     end
   end
 
+  def self.get_layout_vars(nav_bar_links=[], side_bar_links=[], breadcrumbs=[])
+    {
+        nav_bar_links: (nav_bar_links.nil?) ? [] : nav_bar_links.compact,
+        side_bar_links: (side_bar_links.nil?) ? [] : side_bar_links.compact,
+        breadcrumbs: (breadcrumbs.nil?) ? [] : breadcrumbs.compact,
+    }
+  end
+
   # Recursively defines routes for the web service according to the commands included in the given base Module.
   # Starts at '/'. Additionally defines any default routes required by the web service.
   #
@@ -53,45 +64,29 @@ module Rubysite
   # @return [Array] a String array listing the routes which were defined for the given base Module
   def self.define_routes(base)
     base = Kernel.const_get(base.to_sym) if base.class == String
-    site_map = ['/', '/css', '/help'] + Rubysite.define_module_route(base)
+    default_routes = [
+        {link: "/server", name: "Server Info", doc: "Information about the server this console is running on."},
+        {link: "/help", name: "Help", doc: "Interface documentation"}
+    ]
+    defined_routes = (Rubysite.define_module_route(base).compact + default_routes.map { |item| item[:link] })||[]
 
     Sinatra::Base::get "/?" do
-      layout = {
-          name: "#{base}",
-          doc: "A Rubysite generated service",
-          nav_entries: [
-              {link: '/', link_name: 'Home', doc: 'Home'},
-              {link: "/#{base}", link_name: "#{base}", doc: Rubycom::Documentation.get_module_doc(base)},
-              {link: "/help", link_name: "Help", doc: 'Interface documentation'}
-          ]
-      }
-      erb(:index, locals: {layout: layout})
+
+      erb(:index, locals: {layout: Rubysite.get_layout_vars(default_routes)})
     end
 
-    Sinatra::Base::get "/css/:css_name?" do |css_name|
-      css_sym = css_name.to_sym rescue :default
-      css_file = settings.css_files[css_sym] || settings.css_files[:default]
-      File.read(css_file)
+    Sinatra::Base::get "/server" do
+      server_info = {
+          name: "Default Server"
+      }
+      erb(:server_info, locals: {layout: Rubysite.get_layout_vars(default_routes), server_info: server_info})
     end
 
     Sinatra::Base::get "/help" do
-      layout = {
-          name: "Help",
-          back_link: "/",
-          doc: "Help page",
-          nav_entries: [
-              {link: '/', link_name: 'Home', doc: 'Home'},
-              {link: "/#{base}", link_name: "#{base}", doc: Rubycom::Documentation.get_module_doc(base)},
-              {link: "/help", link_name: "Help", doc: 'Interface documentation'}
-          ]
-      }
-      help = {
-          site_map: site_map.flatten
-      }
-      erb(:help, locals: {layout: layout, help: help})
+      erb(:help, locals: {layout: Rubysite.get_layout_vars(default_routes), site_map: defined_routes})
     end
 
-    site_map.flatten
+    defined_routes
   end
 
   # Recursively defines routes for the web service according to the commands included in the given base Module.
@@ -102,6 +97,7 @@ module Rubysite
   # @return [Array] a String array listing the routes which were defined for the given base Module
   def self.define_module_route(base, route_prefix='/')
     base = Kernel.const_get(base.to_sym) if base.class == String
+    #TODO update these to {:link,:name,:doc}
     defined_routes = ["#{route_prefix.chomp('/')}/#{base.to_s}"]
     route_parts = defined_routes[0].split('/')
     commands = Rubycom::Commands.get_top_level_commands(base).select { |sym| sym != :Rubysite } || []
@@ -120,19 +116,17 @@ module Rubysite
     } || []
 
     Sinatra::Base::get "#{defined_routes[0]}/?" do
-      layout = {
+      command_list = {
           name: "#{base}",
           back_link: route_prefix,
-          bread_crumbs: route_parts.each_with_index.map{|_,index| route_parts[0..index].join('/')}.select{|p|!p.empty?},
           doc: Rubycom::Documentation.get_module_doc(base.to_s),
+          bread_crumbs: route_parts.each_with_index.map { |_, index| route_parts[0..index].join('/') }.select { |p| !p.empty? },
           nav_entries: [
               {link: route_prefix, link_name: route_prefix.split('/').last || 'Home', doc: 'Back'},
-          ] + commands + [{link: "/help", link_name: "Help", doc: 'Interface documentation'}]
-      }
-      command_list = {
+          ] + commands + [{link: "/help", link_name: "Help", doc: 'Interface documentation'}],
           command_list: commands
       }
-      erb(:"module/command_list", locals: {layout: layout, command_list: command_list})
+      erb(:"module/command_list", locals: {command_list: command_list})
     end
 
     defined_routes.flatten
@@ -156,9 +150,10 @@ module Rubysite
 
       if params.nil? || params.empty?
         layout = {
+            app_name: settings.app_name,
             name: "#{base}",
             back_link: route_prefix,
-            bread_crumbs: route_parts.each_with_index.map{|_,index| route_parts[0..index].join('/')}.select{|p|!p.empty?},
+            bread_crumbs: route_parts.each_with_index.map { |_, index| route_parts[0..index].join('/') }.select { |p| !p.empty? },
             nav_entries: [
                 {link: route_prefix, link_name: route_prefix.split('/').last || 'Home', doc: 'Parent Module'},
                 {link: "/help", link_name: "Help", doc: 'Interface documentation'}
@@ -173,12 +168,12 @@ module Rubysite
             name: "#{defined_route}_form",
             action: "#{defined_route}",
             method: 'get',
-            fields: param_defs.map{|key, val_hsh|
+            fields: param_defs.map { |key, val_hsh|
               {
-                  label: key.to_s.split('_').map{|word| word.capitalize }.join(' ')+':',
-                  type: (docs[:param].nil?)? 'text' : docs[:param].map{|str| {type: str.match(/\[\w+\]/), name: str.gsub(/\[\w+\]/, '').split(' ').first }}.reduce(&:merge),
+                  label: key.to_s.split('_').map { |word| word.capitalize }.join(' ')+':',
+                  type: (docs[:param].nil?) ? 'text' : docs[:param].map { |str| {type: str.match(/\[\w+\]/), name: str.gsub(/\[\w+\]/, '').split(' ').first} }.reduce(&:merge),
                   name: "#{key.to_s}",
-                  value: (val_hsh[:default] == :nil_rubycom_required_param)? '' : val_hsh[:default]
+                  value: (val_hsh[:default] == :nil_rubycom_required_param) ? '' : val_hsh[:default]
               }
             }
         }
@@ -203,6 +198,7 @@ module Rubysite
           puts Rubycom.call_method(base, command, method_call_params)
 
           layout = {
+              app_name: settings.app_name,
               name: "#{base}",
               back_link: route_prefix,
               nav_entries: [
@@ -223,9 +219,10 @@ module Rubysite
 
         rescue Exception => e
           layout = {
+              app_name: settings.app_name,
               name: "#{base}",
               back_link: route_prefix,
-              bread_crumbs: route_parts.each_with_index.map{|_,index| route_parts[0..index].join('/')}.select{|p|!p.empty?},
+              bread_crumbs: route_parts.each_with_index.map { |_, index| route_parts[0..index].join('/') }.select { |p| !p.empty? },
               nav_entries: [
                   {link: route_prefix, link_name: route_prefix.split('/').last || 'Home', doc: 'Parent Module'},
                   {link: "/help", link_name: "Help", doc: 'Interface documentation'}
